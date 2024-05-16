ecc2ce01351933f983b67f36931a5e6647e11f72
Angel Mansilla Puerto <angelmansillapuerto@outlook.com>
2024-05-16 13:25:03 +0200

    Varios parametros @PathVariable

--- src/main/java/com/angel/curso/springboot/webapp/springbootweb/controllers/PathVaribleController.java
+++ src/main/java/com/angel/curso/springboot/webapp/springbootweb/controllers/PathVaribleController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.angel.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,4 +24,13 @@ public class PathVaribleController {
         return param;
     }
     
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable long id){
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id); 
+
+        return json;
+    }
 }
